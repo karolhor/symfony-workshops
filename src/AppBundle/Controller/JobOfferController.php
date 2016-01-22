18564183c3282f2eff3b8bcaa8c7dc09ee310bb3
Karol Horowski <karol.hor@gmail.com>
2016-01-22 14:59:44 +0100

    job form template in controller

--- src/AppBundle/Controller/JobOfferController.php
+++ src/AppBundle/Controller/JobOfferController.php
@@ -2,8 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Job;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 class JobOfferController extends Controller
@@ -19,9 +24,30 @@ class JobOfferController extends Controller
     /**
      * @Route("/job/new", name="newJob")
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
-        return $this->render('jobOffer/new.html.twig');
+        $job = new Job();
+
+        $form = $this->createFormBuilder($job)
+            ->add('title', TextType::class)
+            ->add('employer', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('type', TextType::class)
+            ->add('attachment', FileType::class)
+            ->add('attachmentName', TextType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            return $this->render('jobOffer/show.html.twig', [
+                'job' => $job
+            ]);
+        }
+
+        return $this->render('jobOffer/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
@@ -29,6 +55,8 @@ class JobOfferController extends Controller
      */
     public function showAction()
     {
-        return $this->render('jobOffer/show.html.twig');
+        return $this->render('jobOffer/show.html.twig', [
+            'job' => new Job()
+        ]);
     }
 }
---
+++ src/AppBundle/Entity/Job.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Class Job
+ */
+class Job
+{
+    /** @var  string */
+    private $title;
+
+    /** @var  string */
+    private $employer;
+
+    /** @var string */
+    private $description;
+
+    /** @var  string */
+    private $type;
+
+    private $attachment;
+
+    /** @var  string */
+    private $attachmentName;
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmployer()
+    {
+        return $this->employer;
+    }
+
+    /**
+     * @param string $employer
+     */
+    public function setEmployer($employer)
+    {
+        $this->employer = $employer;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAttachment()
+    {
+        return $this->attachment;
+    }
+
+    /**
+     * @param mixed $attachment
+     */
+    public function setAttachment($attachment)
+    {
+        $this->attachment = $attachment;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAttachmentName()
+    {
+        return $this->attachmentName;
+    }
+
+    /**
+     * @param string $attachmentName
+     */
+    public function setAttachmentName($attachmentName)
+    {
+        $this->attachmentName = $attachmentName;
+    }
+}
